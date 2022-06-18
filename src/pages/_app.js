b9b0337bcbb66ec041d78c54c3a06da40e56642d
Amri Karisma <amrikarisma@yahoo.com>
2022-06-18 21:52:25 +0700

    fix function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,29 @@
 import 'tailwindcss/tailwind.css'
 import 'styles/style.scss'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+
 import * as ga from '../lib/ga'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
-const router = useRouter()
+function App({ Component, pageProps }) {
+    const router = useRouter()
 
-useEffect(() => {
-    const handleRouteChange = url => {
-        ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+    useEffect(() => {
+        const handleRouteChange = url => {
+            ga.pageview(url)
+        }
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-        router.events.off('routeChangeComplete', handleRouteChange)
-    }
-}, [router.events])
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
+    return <Component {...pageProps} />
+}
 
 export default App
