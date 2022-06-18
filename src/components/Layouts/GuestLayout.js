94753645e96119bfbd76699da1d0b34513a2f59c
Amri Karisma <amrikarisma@yahoo.com>
2022-06-18 21:59:20 +0700

    update title guest

--- src/components/Layouts/GuestLayout.js
+++ src/components/Layouts/GuestLayout.js
@@ -4,7 +4,7 @@ const GuestLayout = ({ children }) => {
     return (
         <div>
             <Head>
-                <title>Laravel</title>
+                <title>Uangku Web - Catatan Keuanganku</title>
             </Head>
 
             <div className="font-sans text-gray-900 antialiased">
