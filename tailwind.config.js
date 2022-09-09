cac56af99ca5c1a6a027ea93dadcf2253b32c407
Amri Karisma <amrikarisma@live.com>
2022-09-09 18:10:08 +0800

    update all file js to jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    content: ['./src/**/*.js'],
+    content: ['./src/**/*.jsx'],
     darkMode: 'media',
     theme: {
         extend: {
